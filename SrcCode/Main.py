d5a11e1203280766422be35e59224a259a35da73
Ririto Ninigaya <riritoninigaya@hotmail.com>
2024-02-26 12:36:26 +0200

    Add files via upload

---
+++ SrcCode/Main.py
@@ -0,0 +1,6 @@
+from RomeTWHotfix import Hotfix
+
+def Main():
+    Hotfix.MouseFixAndKeyboard()
+if __name__ == "__main__":
+    Main()
---
+++ SrcCode/RomeTWHotfix.py
@@ -0,0 +1,6 @@
+import subprocess
+
+class Hotfix:
+    def MouseFixAndKeyboard():
+        # This is Fix Mouse Glitch while Anticheat is not properly working in 2024 year :D
+        subprocess.Popen(args=["start RunAsDate.exe 02\\01\\2020 /immediate 'E:\\Games\\Total War Rome Remastered\\Total War ROME REMASTERED.exe'"])
